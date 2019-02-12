4235873d26d735e9740d9b52ad05d35b9560eaf5
qc_6fe293ba796e462b96adc4bb085d6ddf <qc_6fe293ba796e462b96adc4bb085d6ddf@git.code.tencent.com>
2019-02-12 17:32:34 +0800

    在dev分支上提交变动

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main(int argc, char* argv[]) {
     std::cout << "Hello world!" << std::endl;
     std::cout << "Welcome!" << std::endl;
+    std::cout << "this is branch dev" << std::endl;
     return 0;
 }

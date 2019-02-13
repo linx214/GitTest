cd334a548bc0fc807aab47aafa52576f41a6fab6
qc_6fe293ba796e462b96adc4bb085d6ddf <qc_6fe293ba796e462b96adc4bb085d6ddf@git.code.tencent.com>
2019-02-13 16:36:25 +0800

    从test分支上进行提交

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char* argv[]) {
     std::cout << "Hello world!" << std::endl;
     std::cout << "Welcome!" << std::endl;
     std::cout << "this is branch dev" << std::endl;
+    std::cout << "added from branch test" << std::endl;
     return 0;
 }
